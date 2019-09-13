7869780111f4146bdd3af6f39f314401d711620d
Andrew <kpi_lithium@ukr.net>
2019-09-13 15:21:43 +0300

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AndreyLM/test-modules
+
+go 1.12
---
+++ main.go
@@ -0,0 +1,8 @@
+package testmodules
+
+import "fmt"
+
+// Hi - say hi
+func Hi(name string) string {
+	return fmt.Sprintf("hi, %s", name)
+}
